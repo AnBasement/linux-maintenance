6ea83b8f6192b8914bbf8de9a314cd6d5392c261
AnBasement <KristofferAG@gmail.com>
2025-10-03 23:05:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Linux Maintenance Reminder
+
+This is a simple program that sends scheduled reminders to perform certain actions when running a Linux distribution.
---
+++ maintenance.py
@@ -0,0 +1,5 @@
+print("Checklist for Linux Maintenance: \n"
+      "1. Update package lists and upgrade installed packages.\n"
+      "2. Clean up unnecessary packages and dependencies.\n"
+      "3. Check for and install security updates.\n"
+      "4. Review and clean log files.\n")
